2febe14afc4a206babb0feebed72020da018dddd
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2026-01-31 20:51:14 +0530

    updated to handle the non square matrices DSA(arrays): transpose matrix

--- dsa/01-arrays/transposeMatrix.js
+++ dsa/01-arrays/transposeMatrix.js
@@ -39,13 +39,18 @@
  * @return {void} Modifies matrix in-place
  */
 function transposeMatix(matrix) {
-  for (let i = 0; i < Math.ceil(matrix.length / 2); i++) {
-    for (let j = i + 1; j < matrix[i].length; j++) {
-      const temp = matrix[i][j];
-      matrix[i][j] = matrix[j][i];
-      matrix[j][i] = temp;
+  const rows = matrix.length;
+    const cols = matrix[0].length;
+
+    const result = Array.from({ length: cols }, () => Array(rows));
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            result[j][i] = matrix[i][j];
+        }
     }
-  }
+
+    return result;
 }
 
 /**
